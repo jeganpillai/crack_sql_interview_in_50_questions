d2122d64685ecaae4e0309e6cbf1d5b560c30f1d
jeganpillai <jegan.velappan@gmail.com>
2024-03-07 18:08:51 -0800

    Create seq01_26_classes_with_5_or_more_students.sql
    Initial version

---
+++ seq01_26_classes_with_5_or_more_students.sql
@@ -0,0 +1,29 @@
+-- Question: Find all the classes that have at least five students.
+-- Video: https://www.youtube.com/watch?v=w5Hyu1bBXLI
+
+Create table If Not Exists Courses (student varchar(255), class varchar(255));
+Truncate table Courses;
+insert into Courses (student, class) values 
+ ('A', 'Math')
+,('B', 'English')
+,('C', 'Math')
+,('D', 'Biology')
+,('E', 'Math')
+,('F', 'Computer')
+,('G', 'Math')
+,('H', 'Math')
+,('I', 'Math');
+
+-- Approach 1: General Group By function 
+select class
+from Courses 
+group by class having count(student) >= 5;
+
+-- Approach 2: Using Windows function 
+with all_courses as (
+select student, class
+      ,count(*) over(partition by class order by student) as cnt
+      from Courses)
+select distinct class from all_courses 
+where cnt >= 5; 
+
