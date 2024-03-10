c702068185eaac631fc5f89632978231a07b4738
jeganpillai <jegan.velappan@gmail.com>
2024-03-09 17:56:57 -0800

    Create seq01_27_find_followers_count.sql
    initial version

---
+++ seq01_27_find_followers_count.sql
@@ -0,0 +1,53 @@
+-- Question: Find the followers count for each user
+-- Video: https://www.youtube.com/watch?v=VIh4uD6YL3k
+
+Create table Followers(user_id int, follower_id int);
+Truncate table Followers;
+insert into Followers (user_id, follower_id) values 
+ (0, 1)
+,(1, 0)
+,(2, 0)
+,(2, 1);
+
+-- General Aggregation function 
+select user_id, count(follower_id) as followers_count 
+from Followers 
+group by 1 order by 1;
+
+-- *** using Windows Function to get second top user with followers count *** 
+-- General approach 
+with all_followers_data as (
+select user_id , count(follower_id) as followers_count 
+       from Followers 
+   group by 1) 
+select user_id, followers_count
+      ,rank() over(order by followers_count desc) as rnk 
+      from all_followers_data;
+
+-- simplified version
+select user_id , count(follower_id) as followers_count 
+      ,rank() over(order by count(follower_id) desc) as rnk
+      from Followers 
+  group by 1;
+
+-- *** Get the third highest user based on followers *** 
+Truncate table Followers;
+insert into Followers (user_id, follower_id) values 
+ (0, 1)
+,(0, 2)
+,(1, 0)
+,(1, 1)
+,(2, 0)
+,(2, 1)
+,(2, 2)
+,(3, 1);
+
+with all_followers_cnt as (
+select user_id , count(follower_id) as followers_count 
+      ,rank() over(order by count(follower_id) desc) as rnk
+      ,dense_rank() over(order by count(follower_id) desc) as drnk
+      from Followers 
+  group by 1) 
+select user_id
+       from all_followers_cnt 
+      where drnk = 3;
