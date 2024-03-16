0111599e71c55d3db0ad596963a9cfdc100fab03
jeganpillai <jegan.velappan@gmail.com>
2024-03-15 17:23:23 -0700

    Update seq01_30_number_of_employees_report_to_each_manager.sql
    updated CTE and subquery table

--- seq01_30_number_of_employees_report_to_each_manager.sql
+++ seq01_30_number_of_employees_report_to_each_manager.sql
@@ -2,6 +2,19 @@
 -- Video: https://www.youtube.com/watch?v=giaTRFlsA1M
 
 -- *** Approach 1: Using CTE/Subquery *** 
+-- CTE table 
+with all_managers_info as (
+select reports_to, count(*) as reports_count, round(avg(age),0) as average_age
+       from Employees 
+       where reports_to is not null 
+    group by 1) 
+select e.employee_id, e.name, m.reports_count, m.average_age
+      from Employees e 
+inner join all_managers_info m 
+       on m.reports_to = e.employee_id 
+ order by 1;
+
+-- Subquery 
 select e.employee_id, e.name, m.reports_count, m.average_age
       from Employees e 
 inner join (select reports_to, count(*) as reports_count, round(avg(age),0) as average_age
