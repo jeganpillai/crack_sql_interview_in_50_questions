bd4ce80886d27a9e512e0e181cd429b2eeb9b4d9
jeganpillai <jegan.velappan@gmail.com>
2024-04-06 23:56:36 -0700

    Create seq01_41_user_with_most_friends.sql
    initial version

---
+++ seq01_41_user_with_most_friends.sql
@@ -0,0 +1,30 @@
+-- Question: Find Users with More Friends
+
+-- English Video: https://www.youtube.com/watch?v=22XEbujviuI
+-- Tamil Video: 
+
+Create table RequestAccepted (requester_id int, accepter_id int, accept_date date);
+Truncate table RequestAccepted;
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values 
+ (1, 2, '2024-04-03')
+,(1, 3, '2024-04-08')
+,(2, 3, '2024-04-08')
+,(3, 4, '2024-04-09');
+
+-- Approach 1: General sql approach 
+with all_users as (
+select user, sum(friends_cnt) as friends_cnt 
+      from (select requester_id as user, count(accepter_id) as friends_cnt from RequestAccepted group by 1
+            union all 
+            select accepter_id as user, count(requester_id) as freinds_cnt from RequestAccepted group by 1) a 
+  group by 1 )
+select user from all_users where friends_cnt = (select max(friends_cnt) from all_users);
+
+-- Approach 2: Clean version 
+with all_users as (
+select user, count(user) as friends_cnt 
+      from (select requester_id as user from RequestAccepted
+            union all 
+            select accepter_id as user from RequestAccepted) a 
+  group by 1 )
+select user from all_users where friends_cnt = (select max(friends_cnt) from all_users);
