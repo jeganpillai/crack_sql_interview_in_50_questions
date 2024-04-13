aa3b2c7b6a83b93b9a9becd2a43cb6265ab67d5e
jeganpillai <jegan.velappan@gmail.com>
2024-04-12 17:17:27 -0700

    Create seq01_44_fix_names_in_dataset.sql
    initial version

---
+++ seq01_44_fix_names_in_dataset.sql
@@ -0,0 +1,23 @@
+-- Question: Fix Names in Dataset
+
+-- English Video: 
+-- Tamil Video: 
+
+Create table Users (user_id int, name varchar(40));
+Truncate table Users;
+insert into Users (user_id, name) values 
+ (1, 'aLice')
+,(2, 'bOB');
+
+-- Approach 1: Using LEFT() and RIGHT() functions 
+select user_id, name
+      ,concat(upper(left(name, 1)), lower(right(name, length(name)-1))) as b 
+      from Users
+
+-- Approach 2: Using SUBSTRING() function 
+select user_id, name
+      ,concat(upper(substr(name, 1,1)) , lower(substr(name,2))) as updated_name  
+      from Users
+
+-- Approach 3: Using INITCAP() function (Only for Oracle and PostgreSQL)
+select user_id, initcap(name) as name from Users;
