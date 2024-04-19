7f41b816178eb94918bddff515eb705d8277ae0f
jeganpillai <jegan.velappan@gmail.com>
2024-04-18 19:07:49 -0700

    Create seq01_47_second_highest_salary.sql

---
+++ seq01_47_second_highest_salary.sql
@@ -0,0 +1,46 @@
+-- Question: Find the Second Highest Salary
+
+-- English Video: 
+-- Tamil Video: 
+
+Create table If Not Exists Employee (Id int, Name varchar(255), Salary int, Department varchar(100));
+
+insert into Employee (Id, Name, Salary, Department) values 
+(1, 'Max', '90000', 'IT')
+,(2, 'Joe', '85000', 'IT')
+, (3, 'Henry', '85000', 'HR')
+, (4, 'Will', '70000', 'IT')
+, (5, 'Janet', '69000', 'IT')
+, (6, 'Sam', '60000', 'HR')
+;
+
+-- Approach 1: n00b format using Subquery
+select max(salary) from Employee where salary
+     < (select max(salary) from Employee where salary
+          < (select max(salary) from Employee where salary
+               < (select max(salary) from Employee where salary
+                    < (select max(salary) from Employee)
+                 )
+            )
+        );
+-- Approach 2: Correlated subquery
+SELECT e.Salary AS SecondHighestSalary
+  FROM Employee e
+ WHERE 3 = (SELECT COUNT(DISTINCT ee.Salary) FROM Employee ee
+             WHERE ee.Salary >= e.Salary);
+
+-- Approach 3: Windows function with subquery format 
+SELECT MAX(e.Salary) AS SecondHighestSalary
+  FROM (SELECT Salary
+              ,DENSE_RANK() OVER(ORDER BY Salary Desc) AS rnk
+          FROM Employee  )e
+         WHERE e.rnk = 2
+
+-- Approach 4: Windows function with CTE table 
+with main_table as (
+SELECT Salary as salary
+      ,DENSE_RANK() OVER(ORDER BY Salary desc) AS rnk
+      FROM Employee) 
+select max(salary) from main_table  
+      where rnk = 2
+      ;
