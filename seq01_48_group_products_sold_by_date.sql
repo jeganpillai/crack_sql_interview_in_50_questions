f9105849f11da0953acb0bc5065e44f244623e09
jeganpillai <jegan.velappan@gmail.com>
2024-04-20 20:19:17 -0700

    Update seq01_48_group_products_sold_by_date.sql

--- seq01_48_group_products_sold_by_date.sql
+++ seq01_48_group_products_sold_by_date.sql
@@ -3,6 +3,17 @@
 -- English Video: 
 -- Tamil Video: 
 
+Create table Activities (sell_date date, product varchar(20));
+Truncate table Activities;
+insert into Activities (sell_date, product) values 
+ ('2020-05-30', 'Headphone')
+,('2020-05-30', 'Basketball')
+,('2020-05-30', 'T-Shirt')
+,('2020-06-01', 'Pencil')
+,('2020-06-01', 'Bible')
+,('2020-06-02', 'Mask')
+,('2020-06-02', 'Mask');
+
 -- Step 1: Rough version 
 select sell_date
       ,count(distinct product) as nuw_sold
